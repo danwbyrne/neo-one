39916af365513ae25b2fffadfff2ba170499ee3b
Daniel Byrne <danwbyrne@gmail.com>
2020-04-23 12:49:27 -0700

    Applying package updates.

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const basePath = path.resolve(__dirname, '.git');
 
+const hooks = {
+  'pre-commit': `rush check && rush lint:staged -p 16 && rush build && GIT_DIR=${basePath} rush change -v`,
+  'post-merge': 'rush install',
+  'post-rewrite': 'rush install',
+  'post-checkout': 'rush install',
+};
+
 module.exports = {
-  hooks: {
-    'pre-commit': `rush check && rush lint:staged -p 16 && rush build && GIT_DIR=${basePath} rush change -v`,
-    'post-merge': 'rush install',
-    'post-rewrite': 'rush install',
-    'post-checkout': 'rush install',
-  },
+  hooks: process.env.SUPPRESS_HOOKS ? {} : hooks,
 };
